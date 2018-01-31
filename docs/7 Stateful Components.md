56f0a3da3c646d29253362493289e2a50360f4eb
Ama Asare <aasare@thoughtworks.com>
2018-01-31 17:08:45 -0600

    Introduce Stateful Components

---
+++ docs/7 Stateful Components.md
@@ -0,0 +1,4 @@
+# Introduce Stateful Components
+
+
+
--- src/SearchComponent.jsx
+++ src/SearchComponent.jsx
@@ -1,12 +1,19 @@
 let React = require('react');
 
 class SearchComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      fruits: ['orange', 'apple', 'apricot', 'watermelon']
+    };
+  }
+
   render() {
     return (
       <div className='search-bar'>
         <input type='text' placeholder='Search'/>
-        <p className='list-item'>orange</p>
-        <p className='list-item'>apple</p>
+        <p className='results'>this.state.fruits</p>
+        {/*Question: why does this print this.state.fruits right to my screen?*/}
       </div>);
   }
 }
