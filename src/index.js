e08fc7516304da558392fa03d8a1ed82008fe5f4
Ama Asare <aasare@thoughtworks.com>
2018-01-31 16:33:52 -0600

    Introduce JSX, React Components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 let React = require('react');
 let ReactDOM = require('react-dom');
 
-ReactDOM.render(React.createElement(
-  'div', {'className': 'search-bar'},
-  React.createElement('input', {'type': 'text', 'placeholder': 'Search'}, null)),
-  document.getElementById('root'));
+class SearchComponent extends React.Component {
+  render() {
+    return (
+      <div className='search-bar'>
+          <input type='text' placeholder='Search'/>
+          <p className='list-item'>orange</p>
+          <p className='list-item'>apple</p>
+      </div>);
+  }
+}
+
+ReactDOM.render(<SearchComponent/>, document.getElementById('root'));
